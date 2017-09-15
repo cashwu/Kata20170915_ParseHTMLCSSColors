aed34edd5a6205a399091e740a3e6c4d40dccbb2
cash <cash@cashwu.com>
2017-09-15 22:44:28 +0800

    input 0A0A0A, FFFFFF test case

--- Kata20170915_ParseHTMLCSSColors/HtmlColorParserTests.cs
+++ Kata20170915_ParseHTMLCSSColors/HtmlColorParserTests.cs
@@ -18,6 +18,18 @@ namespace Kata20170915_ParseHTMLCSSColors
             ColorParserShouldbe(new RGB(1, 1, 1), "#010101");
         }
 
+        [TestMethod]
+        public void input_0A0A0A_should_return_10_10_10()
+        {
+            ColorParserShouldbe(new RGB(10, 10, 10), "#0A0A0A");
+        }
+
+        [TestMethod]
+        public void input_FFFFFF_should_return_255_255_255()
+        {
+            ColorParserShouldbe(new RGB(255, 255, 255), "#FFFFFF");
+        }
+
         private static void ColorParserShouldbe(RGB expected, string color)
         {
             var parser = new HtmlColorParser();
@@ -34,9 +46,9 @@ namespace Kata20170915_ParseHTMLCSSColors
         {
             if (color.StartsWith("#"))
             {
-                var r = byte.Parse(color.Substring(1, 2));
-                var g = byte.Parse(color.Substring(3, 2));
-                var b = byte.Parse(color.Substring(5, 2));
+                var r = Convert.ToByte(color.Substring(1, 2), 16);
+                var g = Convert.ToByte(color.Substring(3, 2), 16);
+                var b = Convert.ToByte(color.Substring(5, 2), 16);
 
                 return new RGB(r, g, b);
             }
