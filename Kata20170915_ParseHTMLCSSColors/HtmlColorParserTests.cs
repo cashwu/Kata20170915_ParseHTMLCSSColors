c04f82c750f155b4e153a2f1d187ea3bfb2dbc02
cash <cash@cashwu.com>
2017-09-15 22:17:43 +0800

    input 000000 test case

---
+++ Kata20170915_ParseHTMLCSSColors/HtmlColorParserTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170915_ParseHTMLCSSColors
+{
+    [TestClass]
+    public class HtmlColorParserTests
+    {
+        [TestMethod]
+        public void input_000000_should_return_0_0_0()
+        {
+            var parser = new HtmlColorParser();
+            var actual = parser.Parse("#000000");
+            var expected = new RGB(0, 0, 0);
+            Assert.AreEqual(expected.r, actual.r);
+            Assert.AreEqual(expected.g, actual.g);
+            Assert.AreEqual(expected.b, actual.b);
+        }
+    }
+
+    public class HtmlColorParser
+    {
+        public RGB Parse(string color)
+        {
+            return new RGB(0, 0, 0);
+        }
+    }
+
+    public class RGB
+    {
+        public readonly byte r;
+        public readonly byte g;
+        public readonly byte b;
+
+        public RGB(byte r, byte g, byte b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+}
